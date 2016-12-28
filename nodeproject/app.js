44543e117db8f91926f252cce6451815404c45e0
Matthias Maisch <defacer@web.de>
2016-12-28 12:30:18 +0000

    fixing style...

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,32 @@
-var aloha = 'Aloha Variables';
-console.log(aloha);
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT;
+var host = process.env.HOST;
+
+app.use(express.static('public'));
+// app.use(express.static('src/views'));
+app.use(express.static('bower_components'));
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+    // res.send('Aloha World!');
+    res.render('index', { 
+        list: ['first val', '2nd val', '3rd val'],
+        nav: [  { Link: 'Services'  , Text: 'Services' }, 
+                { Link: 'Portfolio' , Text: 'Portfolio'}, 
+                { Link: 'About'     , Text: 'About'    }, 
+                { Link: 'Team'      , Text: 'Team'     }, 
+                { Link: 'Contact'   , Text: 'Contact'  }]
+    });
+});
+
+app.get('/routing', function(req, res){
+    res.send('Aloha Routing!');
+});
+
+app.listen(port, host, function(err){
+    console.log('The server is running on port: ' + port);
+});
